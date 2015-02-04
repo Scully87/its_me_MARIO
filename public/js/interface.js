9ed848b962a14b7ff75bc54b4687e7b11d91ca00
Scully87 <scully87.ts@googlemail.com>
2015-02-04 17:34:18 +0000

    indentation

--- public/js/interface.js
+++ public/js/interface.js
@@ -4,32 +4,32 @@ $(document).ready(function() {
 
         switch(parseInt(key.which,10)) {
 
-      // Left Arrow Pressed
-      case 37:
-          $('#mario').animate({left: "-=15px"}, 80);
-        break;    
+          // Left Arrow Pressed
+          case 37:
+              $('#mario').animate({left: "-=15px"}, 80);
+            break;    
 
-      // Up Arrow Pressed
-      case 38:
-          $('#mario').animate({bottom: "+=15px"}, 80);
-        break;
-            
-      // Right Arrow Pressed
-      case 39:
-          $('#mario').animate({left: "+=15px"}, 80);
-        break;
+          // Up Arrow Pressed
+          case 38:
+              $('#mario').animate({bottom: "+=15px"}, 80);
+            break;
+                
+          // Right Arrow Pressed
+          case 39:
+              $('#mario').animate({left: "+=15px"}, 80);
+            break;
 
-      // Down Arrow Pressed
-      case 40:
-          $('#mario').animate({bottom: "-=15px"}, 80);
-        break;
+          // Down Arrow Pressed
+          case 40:
+              $('#mario').animate({bottom: "-=15px"}, 80);
+            break;
 
-      // Space Bar Pressed
-      case 32:
-          $('#mario').animate({bottom: "+=150px"}, 250);
-          $('#mario').animate({bottom: "-=150px"}, 250);
-        break;
-    }    
+          // Space Bar Pressed
+          case 32:
+              $('#mario').animate({bottom: "+=150px"}, 250);
+              $('#mario').animate({bottom: "-=150px"}, 250);
+            break;
+        }    
 
   });
 
@@ -41,11 +41,11 @@ $(document).ready(function() {
     setInterval(function() {
       $('#bird').animate({left: "+=1100px"}, 15000)
       $('#bird').animate({left: "-=1100px"}, 15000)
-      }, 500);
+    }, 500);
 
     setInterval(function() {
       $('#bird1').animate({right: "+=1100px"}, 25000)
       $('#bird1').animate({right: "-=1100px"}, 25000)
-      }, 500);    
+    }, 500);    
 
 });
