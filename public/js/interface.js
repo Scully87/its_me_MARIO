13293e78511ffded04f355dc75bb4f7ff847bd49
Scully87 <scully87.ts@googlemail.com>
2015-02-04 14:16:19 +0000

    added mushy

--- public/js/interface.js
+++ public/js/interface.js
@@ -6,31 +6,33 @@ $(document).ready(function() {
 
       // Left Arrow Pressed
       case 37:
-          $('#mario').animate({left: "-=15px"}, '1000');
+          $('#mario').animate({left: "-=15px"}, 'fast');
         break;    
 
       // Up Arrow Pressed
       case 38:
-          $('#mario').animate({bottom: "+=15px"}, '1000');
+          $('#mario').animate({bottom: "+=15px"}, 'fast');
         break;
             
       // Right Arrow Pressed
       case 39:
-          $('#mario').animate({left: "+=15px"}, '1000');
+          $('#mario').animate({left: "+=15px"}, 'fast');
         break;
 
       // Down Arrow Pressed
       case 40:
-          $('#mario').animate({bottom: "-=15px"}, '1000');
+          $('#mario').animate({bottom: "-=15px"}, 'fast');
         break;
 
       // Space Bar Pressed
       case 32:
-          $('#mario').animate({bottom: "+=100px"}, '1000');
-          $('#mario').animate({bottom: "-=100px"}, '1000');
+          $('#mario').animate({bottom: "+=150px"}, 'fast');
+          $('#mario').animate({bottom: "-=150px"}, 'slow');
         break;
     }    
 
   });
+
+    $('#mush').animate({right: "+=1100px"}, 10000)
 
 });
