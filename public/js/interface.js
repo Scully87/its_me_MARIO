7b4e1d2b0e07b392f390f72ed1be71d0c5759589
Scully87 <scully87.ts@googlemail.com>
2015-02-04 14:21:03 +0000

    speeds

--- public/js/interface.js
+++ public/js/interface.js
@@ -6,33 +6,34 @@ $(document).ready(function() {
 
       // Left Arrow Pressed
       case 37:
-          $('#mario').animate({left: "-=15px"}, 'fast');
+          $('#mario').animate({left: "-=15px"}, 80);
         break;    
 
       // Up Arrow Pressed
       case 38:
-          $('#mario').animate({bottom: "+=15px"}, 'fast');
+          $('#mario').animate({bottom: "+=15px"}, 80);
         break;
             
       // Right Arrow Pressed
       case 39:
-          $('#mario').animate({left: "+=15px"}, 'fast');
+          $('#mario').animate({left: "+=15px"}, 80);
         break;
 
       // Down Arrow Pressed
       case 40:
-          $('#mario').animate({bottom: "-=15px"}, 'fast');
+          $('#mario').animate({bottom: "-=15px"}, 80);
         break;
 
       // Space Bar Pressed
       case 32:
-          $('#mario').animate({bottom: "+=150px"}, 'fast');
-          $('#mario').animate({bottom: "-=150px"}, 'slow');
+          $('#mario').animate({bottom: "+=150px"}, 250);
+          $('#mario').animate({bottom: "-=150px"}, 250);
         break;
     }    
 
   });
 
     $('#mush').animate({right: "+=1100px"}, 10000)
+    $('#mush').animate({right: "-=1100px"}, 10000)
 
 });
